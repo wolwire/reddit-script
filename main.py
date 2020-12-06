f489eb1f358887ec80bc1c4c5903dd748421c252
wolwire <agrawal.mayank1996@gmail.com>
2020-12-06 18:46:04 +0530

    Reddit bot without sentiment analysis

--- main.py
+++ main.py
@@ -4,31 +4,21 @@ import json
 import praw
 import yaml
 import time
-import nltk
-import logging
 import logging.config
+import os
 
-logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+logging.config.fileConfig(dir_path +'/logging.ini', disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
 def load_settings():
-    with open(r'/Users/mayank/reddit-praw-bot/settings.local.yaml') as file:
+    with open(dir_path + "/settings.local.yaml") as file:
         global settings
-        global sentiment_analyzer
         global trigger_phrases
         settings = yaml.load(file, Loader=yaml.FullLoader)
-        sentiment_analyzer = SentimentIntensityAnalyzer()
         trigger_phrases = settings["bot"]["trigger_phrases"]
         return settings
-
-
-def find_sentiment_scores(sentence):
-    scores = sentiment_analyzer.polarity_scores(sentence)
-    return scores
 
 
 def load_reddit_instance():
@@ -52,24 +42,21 @@ def run_bot(reddit_instance, subreddit):
     subreddit_instance = reddit_instance.subreddit(subreddit)
     for comment in subreddit_instance.stream.comments():
         comment_text = comment.body
+        logger.info(comment_text)
         # check if comment is not Null and in correct subreddits and the bot doesn't gets involved in recursion
         if comment_text is not None and comment.subreddit not in settings['subs']['blacklist'] and comment.subreddit in settings['subs']['whitelist'] and comment_text != settings["bot"]["reply_text"]:
            # if triggers are hit start actions and comment
             for x in trigger_phrases:
                 if x in comment_text:
-                    #find sentiment scores
-                    scores = find_sentiment_scores(comment_text)
-                    if scores['compound'] < 0:
-                        reply = settings["bot"]["reply_text"]
-                        reply = reply + f"\npositive|negative|neutral\n-|-|-\n{scores['pos']}|{scores['neg']}|{scores['neu']}"
-                        try:
-                            comment.reply(reply)
-                            time.sleep(1000)
-                            break
-                        except Exception as e:
-                            print(e)
-                            time.sleep(100)
-                            break
+                    reply = settings["bot"]["reply_text"]
+                    try:
+                        comment.reply(reply)
+                        time.sleep(1000)
+                        break
+                    except Exception as e:
+                        print(e)
+                        time.sleep(100)
+                        break
 
 
 if __name__ == '__main__':
